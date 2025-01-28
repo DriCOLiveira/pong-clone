072f3f744614a6ca2814c311ced6f8d6d5475399
dri <palonko2@gmail.com>
2025-01-28 12:49:55 -0300

    added a few small improvements

--- scripts/player2.gd
+++ scripts/player2.gd
@@ -5,15 +5,10 @@ const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_up", "move_down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
 
-	#var collision = move_and_collide(velocity * delta)
-	#if collision:
-		#velocity = velocity.bounce(collision.get_normal())
-	move_and_collide(velocity * delta)
+	move_and_slide()
